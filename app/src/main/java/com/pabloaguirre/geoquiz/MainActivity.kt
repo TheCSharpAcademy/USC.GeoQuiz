c0378111ae9e5d91e054c899be4f893ac693820d
Pablo Aguirre de Souza <paguirre82@hotmail.com>
2026-08-10 20:52:16 +1000

    Wiring up buttons to MainActivity.kt

--- app/src/main/java/com/pabloaguirre/geoquiz/MainActivity.kt
+++ app/src/main/java/com/pabloaguirre/geoquiz/MainActivity.kt
@@ -1,16 +1,33 @@
 package com.pabloaguirre.geoquiz
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener { view: View ->
+            // Do something in response to the click here
+        }
+
+        falseButton.setOnClickListener { view: View ->
+            // Do something in response to the click here
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
