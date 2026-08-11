d8feab09b8d672c6b7cc31c82573bc9b84a8b32d
Pablo Aguirre de Souza <paguirre82@hotmail.com>
2026-08-11 18:44:44 +1000

    List of Questions

--- app/src/main/java/com/pabloaguirre/geoquiz/MainActivity.kt
+++ app/src/main/java/com/pabloaguirre/geoquiz/MainActivity.kt
@@ -7,10 +7,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,11 +33,19 @@ class MainActivity : AppCompatActivity() {
         falseButton = findViewById(R.id.false_button)
 
         trueButton.setOnClickListener { view: View ->
-            // Do something in response to the click here
+            Snackbar.make(
+                view,
+                R.string.correct_toast,
+                Snackbar.LENGTH_SHORT
+            ).show()
         }
 
         falseButton.setOnClickListener { view: View ->
-            // Do something in response to the click here
+            Snackbar.make(
+                view,
+                R.string.incorrect_toast,
+                Snackbar.LENGTH_SHORT
+            ).show()
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
