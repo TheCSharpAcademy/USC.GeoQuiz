15da4f90a15f2a4c3b20978235a331d06a24a2c4
Pablo Aguirre de Souza <paguirre82@hotmail.com>
2026-08-15 00:39:52 +1000

    Next button wired up

--- app/src/main/java/com/pabloaguirre/geoquiz/MainActivity.kt
+++ app/src/main/java/com/pabloaguirre/geoquiz/MainActivity.kt
@@ -2,16 +2,15 @@ package com.pabloaguirre.geoquiz
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.pabloaguirre.geoquiz.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var trueButton: Button
-    private lateinit var falseButton: Button
+    private lateinit var binding: ActivityMainBinding
 
     private val questionBank = listOf(
         Question(R.string.question_australia, true),
@@ -27,12 +26,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
-
-        trueButton.setOnClickListener { view: View ->
+        binding.trueButton.setOnClickListener { view: View ->
             Snackbar.make(
                 view,
                 R.string.correct_toast,
@@ -40,13 +37,22 @@ class MainActivity : AppCompatActivity() {
             ).show()
         }
 
-        falseButton.setOnClickListener { view: View ->
+        binding.falseButton.setOnClickListener { view: View ->
             Snackbar.make(
                 view,
                 R.string.incorrect_toast,
                 Snackbar.LENGTH_SHORT
             ).show()
         }
+
+        binding.nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            val questionTextResId = questionBank[currentIndex].textResId
+            binding.questionTextView.setText(questionTextResId)
+        }
+
+        val questionTextResId = questionBank[currentIndex].textResId
+        binding.questionTextView.setText(questionTextResId)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
