b2f648de5731f4d117c3d1f03c4ea86c06eeec28
Pablo Aguirre de Souza <paguirre82@hotmail.com>
2026-08-19 21:25:04 +1000

    grey buttons out and grade score

--- app/src/main/java/com/pabloaguirre/geoquiz/MainActivity.kt
+++ app/src/main/java/com/pabloaguirre/geoquiz/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.pabloaguirre.geoquiz
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.pabloaguirre.geoquiz.databinding.ActivityMainBinding
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -21,9 +24,13 @@ class MainActivity : AppCompatActivity() {
     )
 
     private var currentIndex = 0
+    private val answeredQuestions = BooleanArray(questionBank.size)
+    private var correctAnswerCount = 0
+    private var scoreShown = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
         enableEdgeToEdge()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -61,20 +68,68 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
         binding.questionTextView.setText(questionTextResId)
+        updateAnswerButtons()
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
         val correctAnswer = questionBank[currentIndex].answer
+        val isCorrect = userAnswer == correctAnswer
 
-        val messageResId = if (userAnswer == correctAnswer) {
+        val messageResId = if (isCorrect) {
             R.string.correct_toast
         } else {
             R.string.incorrect_toast
         }
 
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+        if (isCorrect) {
+            correctAnswerCount++
+        }
+        answeredQuestions[currentIndex] = true
+        updateAnswerButtons()
+
+        if (answeredQuestions.all { it } && !scoreShown) {
+            val scorePercentage = correctAnswerCount * 100 / questionBank.size
+            Toast.makeText(
+                this,
+                getString(R.string.quiz_score, scorePercentage),
+                Toast.LENGTH_LONG
+            ).show()
+            scoreShown = true
+        }
+    }
+
+    private fun updateAnswerButtons() {
+        val canAnswer = !answeredQuestions[currentIndex]
+        binding.trueButton.isEnabled = canAnswer
+        binding.falseButton.isEnabled = canAnswer
     }
 }
